fe7833bdb7810c5239f1dcb5364b936be5e609b0
jtrecenti <julio.trecenti@gmail.com>
2019-07-22 08:21:26 -0300

    Initial commit

---
+++ R/soma_um.R
@@ -0,0 +1,12 @@
+#' Somar Um
+#'
+#' Pega o numero de entrada e soma um
+#'
+#' @param x o numero a ser somado de um
+#'
+#' @return o numero somado de um
+#'
+#' @export
+soma_um <- function(x) {
+  x + 1
+}
